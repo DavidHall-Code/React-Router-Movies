caba53c25bac1a3aee639c065aaa81d268fea711
DavidHall-Code <david_m_hall@outlook.com>
2020-10-12 16:36:09 -0400

    Imported from react-router set index.js and app.js

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,6 +1,18 @@
 import React from 'react';
+import {useHistory} from 'react-router-dom';
 
 export default function SavedList(props) {
+  const history = useHistory ();
+  console.log ("history: ", history);
+  const navToHome = (e) => {
+    console.log("Moving to the home page");
+    history.push ('/');
+  }
+  
+  
+  
+  
+  
   return (
     <div className="saved-list">
       <h3>Saved Movies:</h3>
