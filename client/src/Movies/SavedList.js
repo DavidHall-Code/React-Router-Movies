fad22e5beb389799e464a105a163fe832b472c3d
DavidHall-Code <david_m_hall@outlook.com>
2020-10-12 16:53:08 -0400

    Got it working stared working on strech gals to see how far I can take it

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,25 +1,22 @@
 import React from 'react';
-import {useHistory} from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 
-export default function SavedList(props) {
-  const history = useHistory ();
-  console.log ("history: ", history);
+const SavedList = props => {
+  const history = useHistory();
+  console.log("history: ", history);
   const navToHome = (e) => {
-    console.log("Moving to the home page");
-    history.push ('/');
+    console.log("Going to the Home page!!");
+    history.push('/');
   }
-  
-  
-  
-  
-  
   return (
-    <div className="saved-list">
-      <h3>Saved Movies:</h3>
-      {props.list.map(movie => (
-        <span className="saved-movie">{movie.title}</span>
-      ))}
-      <div className="home-button">Home</div>
-    </div>
+  <div className="saved-list">
+    <h3>Saved Movies:</h3>
+    {props.list.map(movie => (
+      <span className="saved-movie">{movie.title}</span>
+    ))}
+    <div className="home-button" onClick={navToHome}>Home</div>
+  </div>
   );
-}
+};
+
+export default SavedList;
