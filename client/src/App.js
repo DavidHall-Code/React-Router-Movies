e4acac38ded27f9d91e8bdcbaf4c343e0379f28e
David Hall <david@pop-os.localdomain>
2020-11-09 15:15:22 -0500

    Worked on APP.JS

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-
-import SavedList from './Movies/SavedList';
+import {Link, Route, Switch} from 'react-router-dom'
+import MovieList from './Movies/MovieList'
+import SavedList from './Movies/SavedList'
+import Movie from './Movies/Movie'
 
 export default function App () {
   const [saved, setSaved] = useState([]); // Stretch: the ids of "saved" movies
@@ -14,6 +16,8 @@ export default function App () {
         .then(response => {
           // Study this response with a breakpoint or log statements
           // and set the response data as the 'movieList' slice of state
+          setMovieList(response.data)
+          console.log(response.data)
         })
         .catch(error => {
           console.error('Server Error', error);
